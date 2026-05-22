e47824147b3ca25c98ac7566adca72ba1a04ebd5
Tran Minh Tri <minhtri084038@gmail.com>
2026-05-22 21:22:40 +0700

    merge main

--- frontend/web/src/layouts/MainLayout.tsx
+++ frontend/web/src/layouts/MainLayout.tsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom';
 import MainHeader from '../components/client/MainHeader';
 import MainFooter from '../components/client/MainFooter';
 import Chatbot from '../components/client/Chatbot';
+import NotificationWebSocketListener from '../components/client/NotificationWebSocketListener';
 
 const MainLayout: React.FC = () => {
   
   return (
     <div className="min-h-screen bg-alpha-dark flex flex-col">
+      <NotificationWebSocketListener />
       <MainHeader />
       <main className="flex-grow">
         <Outlet />
